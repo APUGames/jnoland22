c07f7aee917265880214b293d05a9a46992e58ee
Jesus Noland <jesus.noland@litpen.com>
2023-01-12 23:31:16 -0800

    First course code commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿/**
+ * Author: Jesus Noland
+ * Creation date: 01-09-2023
+ * Purpose: Class 1 C# concepts
+ */
+using System;
+
+namespace HangTheMan
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables needed in Main
+            bool gameOver = false;
+            bool playerWin;
+
+            string playerInput = "my name is george";
+            Console.WriteLine("Welcome to Hang the " + playerInput);
+
+            // This will not be compiled
+            Console.WriteLine("Is the sky blue? " + gameOver);
+
+            playerWin = true;
+            Console.WriteLine("You alive? " + playerWin);
+        }
+    }
+}
