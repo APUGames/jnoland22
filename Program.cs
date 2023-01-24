07ae819255f7663bfc890548885772988c7272ca
Jesus Noland <jesus.noland@litpen.com>
2023-01-23 21:12:04 -0800

    Second class updates

--- Program.cs
+++ Program.cs
@@ -11,18 +11,74 @@ namespace HangTheMan
     {
         static void Main(string[] args)
         {
-            // Variables needed in Main
-            bool gameOver = false;
-            bool playerWin;
+            int GetTotalScore(int generalScore, int bonusScore)
+            {
+                return generalScore + bonusScore;
+            }
 
-            string playerInput = "my name is george";
-            Console.WriteLine("Welcome to Hang the " + playerInput);
+            // Variables needed in Main
+            string gameOver = "true";
+            bool playerWin;
+            int playerScore = 99;
+            int bonus = 50;
+            bonus -= playerScore;
+            bonus++;
+            bonus--;
+
+            for (int i = 150; i > playerScore; i--)
+            {
+                Console.WriteLine("Score: " + i);
+            }
+            int totalScore = GetTotalScore(playerScore, bonus);
+
+            float playerSpeed = 1.56456456f;
+
+            // String array of player choices
+            string[] playerChoices = new string[10];
+            playerChoices[0] = "hello";
+            int playerChoiceIndex = 0;
+
+            // Console.WriteLine("Your total score is " + totalScore);
+
+            string playerInput;
+            // Console.WriteLine("Welcome to Hang the " + playerInput);
 
             // This will not be compiled
-            Console.WriteLine("Is the sky blue? " + gameOver);
+            // Console.WriteLine("Is the sky blue? " + gameOver);
 
-            playerWin = true;
-            Console.WriteLine("You alive? " + playerWin);
+            // playerWin = true;
+            Console.WriteLine("You alive? ");
+
+            bool friend = true;
+            int friendScore = 99;
+            if (!friend)
+            {
+                Console.WriteLine("Boo!");
+                friendScore = 0;
+            }
+            Console.WriteLine(friendScore);
+            while (true)
+            {
+                // do stuff here
+                // Console.WriteLine("You alive? " + playerWin);
+                playerInput = Console.ReadLine();
+                if (playerInput == "quit")
+                {
+                    break;
+                }
+                else
+                {
+                    if (playerInput != null)
+                    {
+                        // 0        1 2 3 4
+                        // [onion][][][][]
+                        playerChoices[playerChoiceIndex] = playerInput;
+                    }
+                    continue;
+                }
+            }
+            Console.WriteLine("Thank you, come again!");
+            Console.WriteLine(playerChoices[playerChoiceIndex]);
         }
     }
 }
